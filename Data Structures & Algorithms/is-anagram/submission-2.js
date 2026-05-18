896843a198818b4b6fc02a9caa3782653470f531
Harsh Patel <harsh710patel@gmail.com>
2026-05-18 18:44:21 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        let seen = new Map();
+
+        for (let i = 0; i < s.length; i++) {
+            let char = s[i];
+            seen.set(char, (seen.get(char) || 0) + 1);
+        }
+
+
+        for(let char of t) {
+            if(seen.get(char) > 0) {
+                seen.set(char, seen.get(char) - 1)                
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
