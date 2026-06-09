2ad310c17f95f22b146728e05a93bfd51a3d3844
Harsh Patel <harsh710patel@gmail.com>
2026-06-09 12:15:52 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+          let seen = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let currentNumber = nums[i];
+
+    if (seen.has(currentNumber)) {
+      return true;
+    } else {
+      seen.set(currentNumber, true);
+    }
+
+  }
+
+    return false;
+
+    }
+}
