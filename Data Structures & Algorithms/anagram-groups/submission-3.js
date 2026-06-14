f9a7859d79a2f44a347bf943e73a25705e9a9243
Harsh Patel <harsh710patel@gmail.com>
2026-06-14 14:11:09 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let seen = new Map();
+
+        for (let i = 0; i < strs.length; i++) {
+            let word = strs[i];
+            console.log(word);
+            let sortedWord = word.split("").sort().join("");
+
+            if (seen.has(sortedWord)) {
+                seen.get(sortedWord).push(word);
+            } else {
+                seen.set(sortedWord, [word]);
+            }
+        }
+
+        return Array.from(seen.values());
+    }
+}
