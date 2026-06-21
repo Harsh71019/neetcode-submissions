47f15aa048ff89eed7b32316adfde6a88d7f0ea0
Harsh Patel <harsh710patel@gmail.com>
2026-06-21 10:54:35 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let seen = new Map();
+
+        for(let i = 0; i < nums.length; i++) {
+            let currentNumber = nums[i];
+
+            if(seen.has(currentNumber)) {
+                return [seen.get(currentNumber), i]
+            } else {
+                let numberToFind = target - currentNumber;
+                seen.set(numberToFind, i)
+            }
+        }
+    }
+}
