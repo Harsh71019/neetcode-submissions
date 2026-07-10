4bd73fa00f033f0cbf161fc16e198da1c09010ec
Harsh Patel <harsh710patel@gmail.com>
2026-07-10 11:53:42 +0530

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) {
+            return false;
+        }
+
+        let seen = new Map();
+
+        for(let char of s) {
+            seen.set(char, (seen.get(char) || 0) + 1);
+        }
+
+        for(let char of t) {
+            if(seen.has(char) && seen.get(char) > 0) {
+                seen.set(char, seen.get(char) - 1)
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
